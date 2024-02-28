214185c23a7566214e81cd9253d30f29bd564ce5
GehanChandima <grakwana@gmail.com>
2024-02-28 16:34:31 +0530

    initial commit

---
+++ src/main/java/com/gcr7/multipledatabaseconnection/MultipleDatabaseConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.gcr7.multipledatabaseconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatabaseConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatabaseConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcr7/multipledatabaseconnection/MultipleDatabaseConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcr7.multipledatabaseconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDatabaseConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
